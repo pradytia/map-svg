3a0acd6f66c7dd63f00d1097eb96494e8b54d663
pradytia <pradytia@mobilecom.co.id>
2021-10-30 15:47:32 +0700

    upload map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Indonesia from "@svg-maps/indonesia";
+import { SVGMap } from "react-svg-map";
+import "react-svg-map/lib/index.css";
+import {lokasi} from './location_indo';
+import Map from './Map';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React'
+    };
+        // Create new map object
+        // this.customIndo = {
+        //     ...Indonesia,
+        //     label: 'Custom map label',
+        //     locations: Indonesia.locations.map((location) => {
+        //         // Modify each location
+        //         console.log(location)
+        //     })
+        // };
+  }
+ 
+  render() {
+    return (
+      // <SVGMap map={lokasi} />
+      <Map/>
+    );
+  }
 }
 
 export default App;
